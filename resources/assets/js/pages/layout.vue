1df25b9d7068f675ef3a815cc9c904bb68924abc
yangliu <yangliu@yangliudeMacBook-Pro.local>
2019-03-13 17:58:43 +0800

    first commit

---
+++ resources/assets/js/pages/layout.vue
@@ -0,0 +1,25 @@
+<style>
+
+</style>
+
+<template>
+  <div id="app-layout">
+    <navigation></navigation>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  import Navigation from '../components/global/Navigation.vue';
+
+  export default {
+    components: {
+      Navigation
+    },
+    created(){
+      this.$store.dispatch( 'loadCafes' );
+      this.$store.dispatch( 'loadUser' );
+      return true;
+    }
+  }
+</script>
